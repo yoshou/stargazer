bbc79c674bcf7d59ba78ae0f718150eb9a5e79e8
yoshou <yoshou.dev@gmail.com>
2025-01-11 18:20:50 +0000

    feat: Change to use graph proc

--- app/reconstruction.cpp
+++ app/reconstruction.cpp
@@ -14,9 +14,16 @@
 #include "correspondance.hpp"
 #include "triangulation.hpp"
 #include "multiview_point_data.hpp"
+#include "glm_serialize.hpp"
+
+#include "graph_proc.h"
+#include "graph_proc_cv.h"
+#include "graph_proc_img.h"
 
 #include "voxelpose.hpp"
 #include "mvpose.hpp"
+
+using namespace coalsack;
 
 class SensorServiceImpl final : public stargazer::Sensor::Service
 {
@@ -68,23 +75,8 @@ public:
     }
 };
 
-std::vector<glm::vec3> reconstruct(const std::map<std::string, stargazer::camera_t> &cameras, const std::map<std::string, std::vector<stargazer::point_data>> &frame, glm::mat4 axis)
+static std::vector<glm::vec3> reconstruct(const std::vector<stargazer::camera_t> &camera_list, const std::vector<std::vector<glm::vec2>>& camera_pts, glm::mat4 axis)
 {
-    std::vector<std::vector<glm::vec2>> camera_pts;
-    std::vector<std::string> camera_names;
-    std::vector<stargazer::camera_t> camera_list;
-    for (const auto &[camera_name, camera] : cameras)
-    {
-        std::vector<glm::vec2> pts;
-        for (const auto &pt : frame.at(camera_name))
-        {
-            pts.push_back(pt.point);
-        }
-        camera_pts.push_back(pts);
-        camera_names.push_back(camera_name);
-        camera_list.push_back(camera);
-    }
-
     std::vector<stargazer::reconstruction::node_t> nodes;
     stargazer::reconstruction::adj_list_t adj;
 
@@ -144,54 +136,405 @@ std::vector<glm::vec3> reconstruct(const std::map<std::string, stargazer::camera
     return markers;
 }
 
-epipolar_reconstruction::epipolar_reconstruction()
-    : service(new SensorServiceImpl()), reconstruction_workers(std::make_shared<task_queue<std::function<void()>>>(4)), task_id_gen(std::random_device()()) {}
-epipolar_reconstruction::~epipolar_reconstruction() = default;
-
-void epipolar_reconstruction::push_frame(const frame_type &frame)
+std::vector<glm::vec3> reconstruct(const std::map<std::string, stargazer::camera_t> &cameras, const std::map<std::string, std::vector<stargazer::point_data>> &frame, glm::mat4 axis)
 {
-    if (!running)
+    std::vector<std::vector<glm::vec2>> camera_pts;
+    std::vector<stargazer::camera_t> camera_list;
+    for (const auto &[camera_name, camera] : cameras)
     {
-        return;
-    }
-
-    const auto cloned_frame = frame;
-    const auto task_id = task_id_gen();
-    {
-        std::lock_guard lock(reconstruction_task_wait_queue_mtx);
-        reconstruction_task_wait_queue.push_back(task_id);
-    }
-
-    reconstruction_task_wait_queue_cv.notify_one();
-
-    reconstruction_workers->push_task([cloned_frame, this, task_id]()
-                                      {
-        const auto markers = reconstruct(cameras, cloned_frame, axis);
-
+        std::vector<glm::vec2> pts;
+        for (const auto &pt : frame.at(camera_name))
         {
-            std::unique_lock<std::mutex> lock(reconstruction_task_wait_queue_mtx);
-            reconstruction_task_wait_queue_cv.wait(lock, [&]
-                                                   { return reconstruction_task_wait_queue.front() == task_id; });
-
-            assert(reconstruction_task_wait_queue.front() == task_id);
-            reconstruction_task_wait_queue.pop_front();
-
-            {
-                std::lock_guard lock(markers_mtx);
-                this->markers = markers;
-            }
-
-            service->notify_sphere(markers);
+            pts.push_back(pt.point);
         }
-
-        reconstruction_task_wait_queue_cv.notify_all(); });
+        camera_pts.push_back(pts);
+        camera_list.push_back(camera);
+    }
+    return reconstruct(camera_list, camera_pts, axis);
 }
 
-void epipolar_reconstruction::run()
+struct float2
 {
-    running = true;
-    server_th.reset(new std::thread([this]()
-                                    {
+    float x;
+    float y;
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+        archive(x, y);
+    }
+};
+
+struct float3
+{
+    float x;
+    float y;
+    float z;
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+        archive(x, y, z);
+    }
+};
+
+using float2_list_message = frame_message<std::vector<float2>>;
+using float3_list_message = frame_message<std::vector<float3>>;
+using mat4_message = frame_message<glm::mat4>;
+
+CEREAL_REGISTER_TYPE(float2_list_message)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(coalsack::frame_message_base, float2_list_message)
+
+CEREAL_REGISTER_TYPE(float3_list_message)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(coalsack::frame_message_base, float3_list_message)
+
+CEREAL_REGISTER_TYPE(mat4_message)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(coalsack::frame_message_base, mat4_message)
+
+class callback_node;
+
+class callback_list : public resource_base
+{
+    using callback_func = std::function<void(const callback_node *, std::string, graph_message_ptr)>;
+    std::vector<callback_func> callbacks;
+
+public:
+    virtual std::string get_name() const
+    {
+        return "callback_list";
+    }
+
+    void add(callback_func callback)
+    {
+        callbacks.push_back(callback);
+    }
+
+    void invoke(const callback_node *node, std::string input_name, graph_message_ptr message) const
+    {
+        for (auto &callback : callbacks)
+        {
+            callback(node, input_name, message);
+        }
+    }
+};
+
+class callback_node : public graph_node
+{
+    std::string name;
+
+public:
+    callback_node()
+        : graph_node()
+    {
+    }
+
+    virtual std::string get_proc_name() const override
+    {
+        return "callback_node";
+    }
+
+    void set_name(const std::string &value)
+    {
+        name = value;
+    }
+    std::string get_name() const
+    {
+        return name;
+    }
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+        archive(name);
+    }
+
+    virtual void process(std::string input_name, graph_message_ptr message) override
+    {
+        if (const auto resource = resources->get("callback_list"))
+        {
+            if (const auto callbacks = std::dynamic_pointer_cast<callback_list>(resource))
+            {
+                callbacks->invoke(this, input_name, message);
+            }
+        }
+    }
+};
+
+CEREAL_REGISTER_TYPE(callback_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, callback_node)
+
+class camera_message : public object_message
+{
+    stargazer::camera_t camera;
+
+public:
+    camera_message() : object_message(), camera()
+    {
+    }
+
+    camera_message(const stargazer::camera_t &camera) : object_message(), camera(camera)
+    {
+    }
+
+    stargazer::camera_t get_camera() const
+    {
+        return camera;
+    }
+
+    void set_camera(const stargazer::camera_t &value)
+    {
+        camera = value;
+    }
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+        archive(camera);
+    }
+};
+
+CEREAL_REGISTER_TYPE(camera_message)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(object_message, camera_message)
+
+class epipolar_reconstruct_node : public graph_node
+{
+    mutable std::mutex cameras_mtx;
+    std::map<std::string, stargazer::camera_t> cameras;
+    glm::mat4 axis;
+    graph_edge_ptr output;
+
+public:
+    epipolar_reconstruct_node()
+        : graph_node(), cameras(), axis(), output(std::make_shared<graph_edge>(this))
+    {
+        set_output(output);
+    }
+
+    virtual std::string get_proc_name() const override
+    {
+        return "epipolar_reconstruct_node";
+    }
+
+    template <typename Archive>
+    void serialize(Archive &archive)
+    {
+        archive(cameras, axis);
+    }
+
+    virtual void process(std::string input_name, graph_message_ptr message) override
+    {
+        if (input_name == "cameras")
+        {
+            if (auto camera_msg = std::dynamic_pointer_cast<object_message>(message))
+            {
+                for (const auto &[name, field] : camera_msg->get_fields())
+                {
+                    if (auto camera_msg = std::dynamic_pointer_cast<camera_message>(field))
+                    {
+                        std::lock_guard lock(cameras_mtx);
+                        cameras[name] = camera_msg->get_camera();
+                    }
+                }
+            }
+
+            return;
+        }
+        if (input_name == "axis")
+        {
+            if (auto mat4_msg = std::dynamic_pointer_cast<mat4_message>(message))
+            {
+                axis = mat4_msg->get_data();
+            }
+
+            return;
+        }
+
+        if (auto obj_msg = std::dynamic_pointer_cast<object_message>(message))
+        {
+            std::vector<std::vector<glm::vec2>> camera_pts;
+            std::vector<stargazer::camera_t> camera_list;
+
+            std::map<std::string, stargazer::camera_t> cameras;
+            {
+                std::lock_guard lock(cameras_mtx);
+                cameras = this->cameras;
+            }
+
+            for (const auto &[name, field] : obj_msg->get_fields())
+            {
+                if (auto points_msg = std::dynamic_pointer_cast<float2_list_message>(field))
+                {
+                    const auto &camera = cameras.at(name);
+                    std::vector<glm::vec2> pts;
+                    for (const auto &pt : points_msg->get_data())
+                    {
+                        pts.push_back(glm::vec2(pt.x, pt.y));
+                    }
+                    camera_pts.push_back(pts);
+                    camera_list.push_back(camera);
+                }
+            }
+
+            const auto markers = reconstruct(camera_list, camera_pts, axis);
+
+            auto marker_msg = std::make_shared<float3_list_message>();
+            std::vector<float3> marker_data;
+            for (const auto &marker : markers)
+            {
+                marker_data.push_back({marker.x, marker.y, marker.z});
+            }
+            marker_msg->set_data(marker_data);
+            output->send(marker_msg);
+        }
+    }
+};
+
+CEREAL_REGISTER_TYPE(epipolar_reconstruct_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, epipolar_reconstruct_node)
+
+class epipolar_reconstruction_pipeline
+{
+    graph_proc graph;
+
+    std::atomic_bool running;
+    std::shared_ptr<std::thread> server_th;
+    std::unique_ptr<grpc::Server> server;
+    std::unique_ptr<SensorServiceImpl> service;
+
+    mutable std::mutex markers_mtx;
+    std::vector<glm::vec3> markers;
+    std::vector<std::function<void(const std::vector<glm::vec3> &)>> markers_received;
+
+    std::shared_ptr<epipolar_reconstruct_node> reconstruct_node;
+    std::shared_ptr<graph_node> input_node;
+
+public:
+    void add_markers_received(std::function<void(const std::vector<glm::vec3> &)> f)
+    {
+        std::lock_guard lock(markers_mtx);
+        markers_received.push_back(f);
+    }
+
+    void clear_markers_received()
+    {
+        std::lock_guard lock(markers_mtx);
+        markers_received.clear();
+    }
+
+    epipolar_reconstruction_pipeline()
+        : graph(), running(false), server_th(), server(), service(std::make_unique<SensorServiceImpl>()), markers(), markers_received(), reconstruct_node(), input_node()
+    {
+    }
+
+    void set_camera(const std::string &name, const stargazer::camera_t &camera)
+    {
+        auto camera_msg = std::make_shared<camera_message>(camera);
+        camera_msg->set_camera(camera);
+
+        auto obj_msg = std::make_shared<object_message>();
+        obj_msg->add_field(name, camera_msg);
+
+        if (reconstruct_node)
+        {
+            graph.process(reconstruct_node.get(), "cameras", obj_msg);
+        }
+    }
+
+    void set_axis(const glm::mat4 &axis)
+    {
+        auto mat4_msg = std::make_shared<mat4_message>();
+        mat4_msg->set_data(axis);
+
+        if (reconstruct_node)
+        {
+            graph.process(reconstruct_node.get(), "axis", mat4_msg);
+        }
+    }
+    
+    using frame_type = std::map<std::string, std::vector<stargazer::point_data>>;
+
+    void push_frame(const frame_type &frame)
+    {
+        if (!running)
+        {
+            return;
+        }
+        
+        auto msg = std::make_shared<object_message>();
+        for (const auto &[name, field] : frame)
+        {
+            auto float2_msg = std::make_shared<float2_list_message>();
+            std::vector<float2> float2_data;
+            for (const auto &pt : field)
+            {
+                float2_data.push_back({pt.point.x, pt.point.y});
+            }
+            float2_msg->set_data(float2_data);
+            msg->add_field(name, float2_msg);
+        }
+
+        if (input_node)
+        {
+            graph.process(input_node.get(), msg);
+        }
+    }
+
+    void run()
+    {
+        std::shared_ptr<subgraph> g(new subgraph());
+
+        std::shared_ptr<fifo_node> n0(new fifo_node());
+        g->add_node(n0);
+
+        input_node = n0;
+
+        std::shared_ptr<epipolar_reconstruct_node> n1(new epipolar_reconstruct_node());
+        n1->set_input(n0->get_output());
+        g->add_node(n1);
+
+        reconstruct_node = n1;
+
+        std::shared_ptr<callback_node> n2(new callback_node());
+        n2->set_input(n1->get_output());
+        g->add_node(n2);
+
+        n2->set_name("markers");
+
+        const auto callbacks = std::make_shared<callback_list>();
+
+        callbacks->add([this](const callback_node *node, std::string input_name, graph_message_ptr message)
+                       {
+            if (node->get_name() == "markers")
+            {
+                if (const auto markers_msg = std::dynamic_pointer_cast<float3_list_message>(message))
+                {
+                    std::vector<glm::vec3> markers;
+                    for (const auto &marker : markers_msg->get_data())
+                    {
+                        markers.push_back(glm::vec3(marker.x, marker.y, marker.z));
+                    }
+
+                    {
+                        std::lock_guard lock(markers_mtx);
+                        this->markers = markers;
+                    }
+
+                    for (const auto &f : markers_received)
+                    {
+                        f(markers);
+                    }
+
+                    service->notify_sphere(markers);
+                }
+            } });
+
+        graph.deploy(g);
+        graph.get_resources()->add(callbacks);
+        graph.run();
+
+        running = true;
+        server_th.reset(new std::thread([this]()
+                                        {
         std::string server_address("0.0.0.0:50051");
 
         grpc::ServerBuilder builder;
@@ -200,29 +543,92 @@ void epipolar_reconstruction::run()
         server = builder.BuildAndStart();
         spdlog::info("Server listening on " + server_address);
         server->Wait(); }));
+    }
+
+    void stop()
+    {
+        if (running.load())
+        {
+            running.store(false);
+            server->Shutdown();
+            if (server_th && server_th->joinable())
+            {
+                server_th->join();
+            }
+        }
+
+        graph.stop();
+    }
+
+    std::vector<glm::vec3> get_markers() const
+    {
+        std::vector<glm::vec3> result;
+
+        {
+            std::lock_guard lock(markers_mtx);
+            result = this->markers;
+        }
+
+        return result;
+    }
+};
+
+class epipolar_reconstruction::impl
+{
+public:
+    std::shared_ptr<epipolar_reconstruction_pipeline> pipeline;
+
+    impl()
+        : pipeline(std::make_shared<epipolar_reconstruction_pipeline>())
+    {
+    }
+
+    ~impl() = default;
+
+    void run()
+    {
+        pipeline->run();
+    }
+
+    void stop()
+    {
+        pipeline->stop();
+    }
+};
+
+epipolar_reconstruction::epipolar_reconstruction()
+    : pimpl(new impl()) {}
+epipolar_reconstruction::~epipolar_reconstruction() = default;
+
+void epipolar_reconstruction::push_frame(const frame_type &frame)
+{
+    pimpl->pipeline->push_frame(frame);
+}
+
+void epipolar_reconstruction::run()
+{
+    pimpl->run();
 }
 
 void epipolar_reconstruction::stop()
 {
-    if (running.load())
-    {
-        running.store(false);
-        server->Shutdown();
-        if (server_th && server_th->joinable())
-        {
-            server_th->join();
-        }
-    }
+    pimpl->stop();
 }
 
 std::vector<glm::vec3> epipolar_reconstruction::get_markers() const
 {
-    std::vector<glm::vec3> result;
-    {
-        std::lock_guard lock(markers_mtx);
-        result = markers;
-    }
-    return result;
+    return pimpl->pipeline->get_markers();
+}
+
+void epipolar_reconstruction::set_camera(const std::string &name, const stargazer::camera_t &camera)
+{
+    pimpl->pipeline->set_camera(name, camera);
+    multiview_point_reconstruction::set_camera(name, camera);
+}
+void epipolar_reconstruction::set_axis(const glm::mat4 &axis)
+{
+    pimpl->pipeline->set_axis(axis);
+    multiview_point_reconstruction::set_axis(axis);
 }
 
 #include <cereal/types/array.hpp>
---
+++ lib/glm_serialize.hpp
@@ -0,0 +1,59 @@
+#ifndef GLM_SERIALIZE_HPP
+#define GLM_SERIALIZE_HPP
+
+#include <cereal/cereal.hpp>
+#include <glm/glm.hpp>
+
+namespace glm {
+
+template <class Archive, typename T, glm::precision P>
+void save(Archive& ar, const glm::tvec2<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y));
+}
+
+template <class Archive, typename T, glm::precision P>
+void load(Archive& ar, glm::tvec2<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y));
+}
+
+template <class Archive, typename T, glm::precision P>
+void save(Archive& ar, const glm::tvec3<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y), cereal::make_nvp("z", vec.z));
+}
+
+template <class Archive, typename T, glm::precision P>
+void load(Archive& ar, glm::tvec3<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y), cereal::make_nvp("z", vec.z));
+}
+
+template <class Archive, typename T, glm::precision P>
+void save(Archive& ar, const glm::tvec4<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y), cereal::make_nvp("z", vec.z), cereal::make_nvp("w", vec.w));
+}
+
+template <class Archive, typename T, glm::precision P>
+void load(Archive& ar, glm::tvec4<T, P>& vec) {
+    ar(cereal::make_nvp("x", vec.x), cereal::make_nvp("y", vec.y), cereal::make_nvp("z", vec.z), cereal::make_nvp("w", vec.w));
+}
+
+template <class Archive, typename T, glm::precision P>
+void save(Archive& ar, const glm::tmat4x4<T, P>& mat) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            ar(cereal::make_nvp("m" + std::to_string(i) + std::to_string(j), mat[i][j]));
+        }
+    }
+}
+
+template <class Archive, typename T, glm::precision P>
+void load(Archive& ar, glm::tmat4x4<T, P>& mat) {
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            ar(cereal::make_nvp("m" + std::to_string(i) + std::to_string(j), mat[i][j]));
+        }
+    }
+}
+
+} // namespace glm
+
+#endif // GLM_SERIALIZE_HPP
