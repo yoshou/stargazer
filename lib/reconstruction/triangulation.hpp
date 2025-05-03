ac460af3ca484ffb449f2d865b0d8adae7ea68de
yoshou <yoshou.dev@gmail.com>
2025-05-03 06:53:26 +0000

    refactor: Apply formatter

--- lib/reconstruction/triangulation.hpp
+++ lib/reconstruction/triangulation.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
-#include <vector>
 #include <glm/glm.hpp>
+#include <vector>
+
 #include "parameters.hpp"
 
-namespace stargazer::reconstruction
-{
-    glm::vec3 triangulate(const glm::vec2 pt1, const glm::vec2 pt2, const camera_t &cm1, const camera_t &cm2);
+namespace stargazer::reconstruction {
+glm::vec3 triangulate(const glm::vec2 pt1, const glm::vec2 pt2, const camera_t &cm1,
+                      const camera_t &cm2);
 
-    glm::vec3 triangulate_undistorted(const glm::vec2 pt1, const glm::vec2 pt2, const camera_t &cm1, const camera_t &cm2);
+glm::vec3 triangulate_undistorted(const glm::vec2 pt1, const glm::vec2 pt2, const camera_t &cm1,
+                                  const camera_t &cm2);
 
-    glm::vec3 triangulate(const std::vector<glm::vec2> &points, const std::vector<camera_t> &cameras);
-}
+glm::vec3 triangulate(const std::vector<glm::vec2> &points, const std::vector<camera_t> &cameras);
+}  // namespace stargazer::reconstruction
