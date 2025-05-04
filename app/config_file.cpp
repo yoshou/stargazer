de8bd21a4ebf12b7448dacf2b3de497cf88118f2
yoshou <yoshou.dev@gmail.com>
2025-05-04 07:50:42 +0000

    feat: Update config file format

--- app/config_file.cpp
+++ app/config_file.cpp
@@ -4,7 +4,96 @@
 #include <nlohmann/json.hpp>
 #include <unordered_set>
 
+namespace nlohmann {
+template <typename... Ts>
+struct adl_serializer<std::variant<Ts...>> {
+  static void to_json(nlohmann::json &j, const std::variant<Ts...> &data) {
+    std::visit([&j](const auto &v) { j = v; }, data);
+  }
+  static void from_json(const nlohmann::json &j, std::variant<Ts...> &data) {
+    if (j.is_string()) {
+      data = j.get<std::string>();
+    } else if (j.is_number_integer()) {
+      data = j.get<std::int64_t>();
+    } else if (j.is_number_float()) {
+      data = j.get<float>();
+    } else if (j.is_boolean()) {
+      data = j.get<bool>();
+    } else {
+      throw std::runtime_error("Invalid variant type");
+    }
+  }
+};
+}  // namespace nlohmann
+
 namespace stargazer {
+static node_type get_node_type(const std::string &type) {
+  if (type == "raspi") {
+    return node_type::raspi;
+  } else if (type == "raspi_color") {
+    return node_type::raspi_color;
+  } else if (type == "rs_d435") {
+    return node_type::rs_d435;
+  } else if (type == "rs_d435_color") {
+    return node_type::rs_d435_color;
+  } else if (type == "depthai_color") {
+    return node_type::depthai_color;
+  } else if (type == "raspi_playback") {
+    return node_type::raspi_playback;
+  } else if (type == "panoptic") {
+    return node_type::panoptic;
+  } else if (type == "record") {
+    return node_type::record;
+  } else if (type == "calibration") {
+    return node_type::calibration;
+  } else if (type == "voxelpose_reconstruction") {
+    return node_type::voxelpose_reconstruction;
+  } else if (type == "mvpose_reconstruction") {
+    return node_type::mvpose_reconstruction;
+  } else if (type == "epipolar_reconstruction") {
+    return node_type::epipolar_reconstruction;
+  } else if (type == "pattern_board_calibration_target_detector") {
+    return node_type::pattern_board_calibration_target_detector;
+  } else if (type == "three_point_bar_calibration_target_detector") {
+    return node_type::three_point_bar_calibration_target_detector;
+  }
+  throw std::runtime_error("Invalid node type");
+}
+
+static std::string get_node_type_name(node_type type) {
+  switch (type) {
+    case node_type::raspi:
+      return "raspi";
+    case node_type::raspi_color:
+      return "raspi_color";
+    case node_type::rs_d435:
+      return "rs_d435";
+    case node_type::rs_d435_color:
+      return "rs_d435_color";
+    case node_type::depthai_color:
+      return "depthai_color";
+    case node_type::raspi_playback:
+      return "raspi_playback";
+    case node_type::panoptic:
+      return "panoptic";
+    case node_type::record:
+      return "record";
+    case node_type::calibration:
+      return "calibration";
+    case node_type::voxelpose_reconstruction:
+      return "voxelpose_reconstruction";
+    case node_type::mvpose_reconstruction:
+      return "mvpose_reconstruction";
+    case node_type::epipolar_reconstruction:
+      return "epipolar_reconstruction";
+    case node_type::pattern_board_calibration_target_detector:
+      return "pattern_board_calibration_target_detector";
+    case node_type::three_point_bar_calibration_target_detector:
+      return "three_point_bar_calibration_target_detector";
+  }
+  throw std::runtime_error("Invalid node type");
+}
+
 configuration_file::configuration_file(const std::string &path) : path(path) {
   std::ifstream ifs;
   ifs.open(path, std::ios::in);
@@ -24,73 +113,16 @@ configuration_file::configuration_file(const std::string &path) : path(path) {
       std::vector<node_info> node_infos;
       for (const auto &j_node : pipeline["nodes"]) {
         node_info node;
-        const auto type = j_node["type"].get<std::string>();
-        if (type == "raspi") {
-          node.type = node_type::raspi;
-        } else if (type == "raspi_color") {
-          node.type = node_type::raspi_color;
-        } else if (type == "rs_d435") {
-          node.type = node_type::rs_d435;
-        } else if (type == "rs_d435_color") {
-          node.type = node_type::rs_d435_color;
-        } else if (type == "depthai_color") {
-          node.type = node_type::depthai_color;
-        } else if (type == "raspi_playback") {
-          node.type = node_type::raspi_playback;
-        } else if (type == "panoptic") {
-          node.type = node_type::panoptic;
-        } else if (type == "record") {
-          node.type = node_type::record;
-        } else if (type == "calibration") {
-          node.type = node_type::calibration;
-        } else if (type == "voxelpose_reconstruction") {
-          node.type = node_type::voxelpose_reconstruction;
-        } else if (type == "mvpose_reconstruction") {
-          node.type = node_type::mvpose_reconstruction;
-        } else if (type == "epipolar_reconstruction") {
-          node.type = node_type::epipolar_reconstruction;
-        } else if (type == "pattern_board_calibration_target_detector") {
-          node.type = node_type::pattern_board_calibration_target_detector;
-        } else if (type == "three_point_bar_calibration_target_detector") {
-          node.type = node_type::three_point_bar_calibration_target_detector;
-        } else {
-          throw std::runtime_error("Invalid node type");
-        }
-
-        node.name = j_node["name"].get<std::string>();
-        if (type == "raspi_playback") {
-          node.id = j_node["id"].get<std::string>();
-          node.db_path = j_node["db_path"].get<std::string>();
-          node.name = j_node["name"].get<std::string>();
-        } else if (type == "panoptic") {
-          node.id = j_node["id"].get<std::string>();
-          node.db_path = j_node["db_path"].get<std::string>();
-          node.name = j_node["name"].get<std::string>();
-        } else if (type == "record") {
-          node.db_path = j_node["db_path"].get<std::string>();
-          node.name = j_node["name"].get<std::string>();
-        } else if (type == "calibration") {
-        } else if (type == "voxelpose_reconstruction") {
-        } else if (type == "mvpose_reconstruction") {
-        } else if (type == "epipolar_reconstruction") {
-        } else if (type == "pattern_board_calibration_target_detector") {
-        } else if (type == "three_point_bar_calibration_target_detector") {
-        } else {
-          node.id = j_node["id"].get<std::string>();
-          node.address = j_node["address"].get<std::string>();
-          node.endpoint = j_node["gateway"].get<std::string>();
-        }
-        if (j_node.contains("params")) {
-          for (const auto &[key, value] : j_node["params"].items()) {
-            if (value.is_number()) {
-              node.params.insert(std::make_pair(key, value.get<float>()));
-            } else if (value.is_boolean()) {
-              node.params.insert(std::make_pair(key, value.get<bool>()));
-            }
+        for (const auto &[key, value] : j_node.items()) {
+          if (key == "type") {
+            node.type = get_node_type(value.get<std::string>());
+          } else if (key == "name") {
+            node.name = value.get<std::string>();
+          } else if (key == "inputs") {
+            node.inputs = value.get<std::unordered_map<std::string, std::string>>();
+          } else {
+            node.params[key] = value.get<node_param_t>();
           }
-        }
-        if (j_node.contains("inputs")) {
-          node.inputs = j_node["inputs"].get<std::unordered_map<std::string, std::string>>();
         }
         node_infos.push_back(node);
 
@@ -112,83 +144,11 @@ void configuration_file::update() {
     for (const auto &node : nodes) {
       nlohmann::json j_node;
 
-      std::string node_type_name;
-      switch (node.type) {
-        case node_type::raspi:
-          node_type_name = "raspi";
-          break;
-        case node_type::raspi_color:
-          node_type_name = "raspi_color";
-          break;
-        case node_type::rs_d435:
-          node_type_name = "rs_d435";
-          break;
-        case node_type::rs_d435_color:
-          node_type_name = "rs_d435_color";
-          break;
-        case node_type::depthai_color:
-          node_type_name = "depthai_color";
-          break;
-        case node_type::raspi_playback:
-          node_type_name = "raspi_playback";
-          break;
-        case node_type::panoptic:
-          node_type_name = "panoptic";
-          break;
-        case node_type::record:
-          node_type_name = "record";
-          break;
-        case node_type::calibration:
-          node_type_name = "calibration";
-          break;
-        case node_type::voxelpose_reconstruction:
-          node_type_name = "voxelpose_reconstruction";
-          break;
-        case node_type::mvpose_reconstruction:
-          node_type_name = "mvpose_reconstruction";
-          break;
-        case node_type::epipolar_reconstruction:
-          node_type_name = "epipolar_reconstruction";
-          break;
-        case node_type::pattern_board_calibration_target_detector:
-          node_type_name = "pattern_board_calibration_target_detector";
-          break;
-        case node_type::three_point_bar_calibration_target_detector:
-          node_type_name = "three_point_bar_calibration_target_detector";
-          break;
-        default:
-          throw std::runtime_error("Invalid node type");
-      }
-
-      j_node["type"] = node_type_name;
+      j_node["type"] = get_node_type_name(node.type);
       j_node["name"] = node.name;
-      nlohmann::json j_params;
+      j_node["inputs"] = node.inputs;
       for (const auto &[key, value] : node.params) {
-        if (std::holds_alternative<float>(value)) {
-          j_params[key] = std::get<float>(value);
-        } else if (std::holds_alternative<bool>(value)) {
-          j_params[key] = std::get<bool>(value);
-        }
-      }
-      j_node["params"] = j_params;
-      if (node.type == node_type::raspi_playback) {
-        j_node["id"] = node.id;
-        j_node["db_path"] = node.db_path;
-      } else if (node.type == node_type::panoptic) {
-        j_node["id"] = node.id;
-        j_node["db_path"] = node.db_path;
-      } else if (node.type == node_type::record) {
-        j_node["db_path"] = node.db_path;
-      } else if (node.type == node_type::calibration) {
-      } else if (node.type == node_type::voxelpose_reconstruction) {
-      } else if (node.type == node_type::mvpose_reconstruction) {
-      } else if (node.type == node_type::epipolar_reconstruction) {
-      } else if (node.type == node_type::pattern_board_calibration_target_detector) {
-      } else if (node.type == node_type::three_point_bar_calibration_target_detector) {
-      } else {
-        j_node["id"] = node.id;
-        j_node["address"] = node.address;
-        j_node["gateway"] = node.endpoint;
+        j_node[key] = value;
       }
       j_nodes.push_back(j_node);
     }
