1c426ca789ede85272cd6451f3154a0c1ffccc87
yoshou <yoshou.dev@gmail.com>
2025-05-04 19:09:53 +0000

    refactor: Rename classes

--- app/reconstruction_pipeline.hpp
+++ app/reconstruction_pipeline.hpp
@@ -11,7 +11,7 @@
 #include "parameters.hpp"
 #include "point_data.hpp"
 
-class multiview_point_reconstruction {
+class multiview_point_reconstruction_pipeline {
   std::map<std::string, stargazer::camera_t> cameras;
   glm::mat4 axis;
   class impl;
@@ -20,8 +20,8 @@ class multiview_point_reconstruction {
   using frame_type = std::map<std::string, std::vector<stargazer::point_data>>;
 
  public:
-  multiview_point_reconstruction();
-  virtual ~multiview_point_reconstruction();
+  multiview_point_reconstruction_pipeline();
+  virtual ~multiview_point_reconstruction_pipeline();
 
   void push_frame(const frame_type &frame);
   void run();
@@ -35,15 +35,15 @@ class multiview_point_reconstruction {
   void set_axis(const glm::mat4 &axis);
 };
 
-class multiview_image_reconstruction {
+class multiview_image_reconstruction_pipeline {
   class impl;
   std::unique_ptr<impl> pimpl;
 
   using frame_type = std::map<std::string, cv::Mat>;
 
  public:
-  multiview_image_reconstruction();
-  virtual ~multiview_image_reconstruction();
+  multiview_image_reconstruction_pipeline();
+  virtual ~multiview_image_reconstruction_pipeline();
 
   void push_frame(const frame_type &frame);
   void run(const std::vector<node_info> &infos);
