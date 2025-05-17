6443405ec8f833e7a7ab777d7dc53fe7b61ea730
yoshou <yoshou.dev@gmail.com>
2025-05-17 15:17:56 +0000

    feat: Change to use graph node

--- app/calibration_pipeline.cpp
+++ app/calibration_pipeline.cpp
@@ -1529,68 +1529,246 @@ static std::vector<size_t> create_random_indices(size_t size) {
   return data;
 }
 
-void intrinsic_calibration_pipeline::push_frame(const std::vector<point_data> &frame) {
-  frames.push_back(frame);
+class intrinsic_calibration_node : public graph_node {
+  std::shared_ptr<graph_edge> output;
+
+  std::vector<std::vector<stargazer::point_data>> frames;
+  stargazer::camera_t initial_camera;
+  stargazer::camera_t calibrated_camera;
+  double rms = 0.0;
+
+ public:
+  intrinsic_calibration_node() : graph_node(), output(std::make_shared<graph_edge>(this)) {
+    set_output(output);
+  }
+
+  virtual std::string get_proc_name() const override { return "intrinsic_calibration_node"; }
+
+  template <typename Archive>
+  void serialize(Archive &archive) {
+    archive(initial_camera);
+  }
+
+  virtual void process(std::string input_name, graph_message_ptr message) override {
+    if (input_name == "calibrate") {
+      if (auto camera_msg = std::dynamic_pointer_cast<camera_message>(message)) {
+        initial_camera = camera_msg->get_camera();
+      }
+
+      calibrate();
+
+      auto camera_msg = std::make_shared<camera_message>(calibrated_camera);
+      output->send(camera_msg);
+    }
+
+    if (auto points_msg = std::dynamic_pointer_cast<float2_list_message>(message)) {
+      std::vector<stargazer::point_data> points;
+      for (const auto &pt : points_msg->get_data()) {
+        points.push_back(stargazer::point_data{glm::vec2(pt.x, pt.y), 0, 0});
+      }
+      push_frame(points);
+    }
+
+    if (auto image_msg = std::dynamic_pointer_cast<image_message>(message)) {
+      const auto &image = image_msg->get_image();
+      cv::Mat img(image.get_height(), image.get_width(), convert_to_cv_type(image.get_format()),
+                  (void *)image.get_data(), image.get_stride());
+      push_frame(img);
+    }
+  }
+
+  double get_rms() const { return rms; }
+
+  void set_initial_camera(const stargazer::camera_t &camera) { initial_camera = camera; }
+
+  const stargazer::camera_t &get_calibrated_camera() const { return calibrated_camera; }
+
+  size_t get_num_frames() const { return frames.size(); }
+
+  void push_frame(const std::vector<stargazer::point_data> &frame) { frames.push_back(frame); }
+
+  void push_frame(const cv::Mat &frame) {
+    std::vector<cv::Point2f> board;
+    if (detect_calibration_board(frame, board)) {
+      std::vector<point_data> points;
+      for (const auto &point : board) {
+        points.push_back(point_data{glm::vec2(point.x, point.y), 0, 0});
+      }
+      push_frame(points);
+    }
+  }
+
+  void calibrate() {
+    const auto image_width = initial_camera.width;
+    const auto image_height = initial_camera.height;
+    const auto square_size = cv::Size2f(2.41, 2.4);  // TODO: Define as config
+    const auto board_size = cv::Size(10, 7);         // TODO: Define as config
+    const auto image_size = cv::Size(image_width, image_height);
+
+    std::vector<std::vector<cv::Point3f>> object_points;
+    std::vector<std::vector<cv::Point2f>> image_points;
+
+    std::vector<cv::Point3f> object_point;
+    calc_board_corner_positions(board_size, square_size, object_point);
+
+    const auto max_num_frames = 100;  // TODO: Define as config
+
+    const auto frame_indices =
+        create_random_indices(std::min(frames.size(), static_cast<size_t>(max_num_frames)));
+
+    for (const auto &frame_index : frame_indices) {
+      const auto &frame = frames.at(frame_index);
+
+      object_points.push_back(object_point);
+
+      std::vector<cv::Point2f> image_point;
+      for (const auto &point : frame) {
+        image_point.push_back(cv::Point2f(point.point.x, point.point.y));
+      }
+
+      image_points.push_back(image_point);
+    }
+
+    std::vector<cv::Mat> rvecs, tvecs;
+    cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
+    cv::Mat dist_coeffs = cv::Mat::zeros(5, 1, CV_64F);
+
+    rms = cv::calibrateCamera(object_points, image_points, image_size, camera_matrix, dist_coeffs,
+                              rvecs, tvecs);
+
+    calibrated_camera.intrin.fx = camera_matrix.at<double>(0, 0);
+    calibrated_camera.intrin.fy = camera_matrix.at<double>(1, 1);
+    calibrated_camera.intrin.cx = camera_matrix.at<double>(0, 2);
+    calibrated_camera.intrin.cy = camera_matrix.at<double>(1, 2);
+    calibrated_camera.intrin.coeffs[0] = dist_coeffs.at<double>(0);
+    calibrated_camera.intrin.coeffs[1] = dist_coeffs.at<double>(1);
+    calibrated_camera.intrin.coeffs[2] = dist_coeffs.at<double>(2);
+    calibrated_camera.intrin.coeffs[3] = dist_coeffs.at<double>(3);
+    calibrated_camera.intrin.coeffs[4] = dist_coeffs.at<double>(4);
+    calibrated_camera.width = image_width;
+    calibrated_camera.height = image_height;
+  }
+};
+
+CEREAL_REGISTER_TYPE(intrinsic_calibration_node)
+CEREAL_REGISTER_POLYMORPHIC_RELATION(graph_node, intrinsic_calibration_node)
+
+class intrinsic_calibration_pipeline::impl {
+ public:
+  graph_proc graph;
+
+  std::atomic_bool running;
+
+  std::shared_ptr<intrinsic_calibration_node> calib_node;
+  std::shared_ptr<graph_node> input_node;
+
+  impl() = default;
+
+  void run(const std::vector<node_info> &infos) {
+    std::shared_ptr<subgraph> g(new subgraph());
+
+    std::shared_ptr<frame_number_numbering_node> n4(new frame_number_numbering_node());
+    g->add_node(n4);
+
+    input_node = n4;
+
+    std::shared_ptr<object_mux_node> n5(new object_mux_node());
+    n5->set_input(n4->get_output());
+    g->add_node(n5);
+
+    for (const auto &info : infos) {
+      if (info.get_type() == node_type::calibration) {
+        std::shared_ptr<intrinsic_calibration_node> n1(new intrinsic_calibration_node());
+        n1->set_input(n5->get_output());
+        g->add_node(n1);
+
+        calib_node = n1;
+      }
+    }
+
+    if (calib_node == nullptr) {
+      spdlog::error("Calibration node not found");
+      return;
+    }
+
+    std::shared_ptr<callback_node> n2(new callback_node());
+    n2->set_input(calib_node->get_output());
+    g->add_node(n2);
+
+    n2->set_name("camera");
+
+    const auto callbacks = std::make_shared<callback_list>();
+
+    callbacks->add(
+        [this](const callback_node *node, std::string input_name, graph_message_ptr message) {});
+
+    graph.deploy(g);
+    graph.get_resources()->add(callbacks);
+    graph.run();
+
+    running = true;
+  }
+
+  void stop() {
+    running.store(false);
+    graph.stop();
+  }
+};
+
+intrinsic_calibration_pipeline::intrinsic_calibration_pipeline()
+    : pimpl(std::make_unique<impl>()) {}
+
+intrinsic_calibration_pipeline::~intrinsic_calibration_pipeline() = default;
+
+void intrinsic_calibration_pipeline::run(const std::vector<node_info> &infos) { pimpl->run(infos); }
+void intrinsic_calibration_pipeline::stop() { pimpl->stop(); }
+
+double intrinsic_calibration_pipeline::get_rms() const {
+  if (pimpl->calib_node) {
+    return pimpl->calib_node->get_rms();
+  }
+  return 0.0;
 }
 
+void intrinsic_calibration_pipeline::set_image_size(int width, int height) {
+  if (pimpl->calib_node) {
+    stargazer::camera_t initial_camera;
+    initial_camera.width = width;
+    initial_camera.height = height;
+    pimpl->calib_node->set_initial_camera(initial_camera);
+  }
+}
+
+const stargazer::camera_t &intrinsic_calibration_pipeline::get_calibrated_camera() const {
+  if (pimpl->calib_node) {
+    return pimpl->calib_node->get_calibrated_camera();
+  }
+  static stargazer::camera_t empty_camera;
+  return empty_camera;
+}
+
+size_t intrinsic_calibration_pipeline::get_num_frames() const {
+  if (pimpl->calib_node) {
+    return pimpl->calib_node->get_num_frames();
+  }
+  return 0;
+}
+
+void intrinsic_calibration_pipeline::push_frame(const std::vector<stargazer::point_data> &frame) {
+  if (pimpl->calib_node) {
+    pimpl->calib_node->push_frame(frame);
+  }
+}
 void intrinsic_calibration_pipeline::push_frame(const cv::Mat &frame) {
-  std::vector<cv::Point2f> board;
-  if (detect_calibration_board(frame, board)) {
-    std::vector<point_data> points;
-    for (const auto &point : board) {
-      points.push_back(point_data{glm::vec2(point.x, point.y), 0, 0});
-    }
-    push_frame(points);
+  if (pimpl->calib_node) {
+    pimpl->calib_node->push_frame(frame);
   }
 }
 
 void intrinsic_calibration_pipeline::calibrate() {
-  const auto square_size = cv::Size2f(2.41, 2.4);  // TODO: Define as config
-  const auto board_size = cv::Size(10, 7);         // TODO: Define as config
-  const auto image_size = cv::Size(image_width, image_height);
-
-  std::vector<std::vector<cv::Point3f>> object_points;
-  std::vector<std::vector<cv::Point2f>> image_points;
-
-  std::vector<cv::Point3f> object_point;
-  calc_board_corner_positions(board_size, square_size, object_point);
-
-  const auto max_num_frames = 100;  // TODO: Define as config
-
-  const auto frame_indices =
-      create_random_indices(std::min(frames.size(), static_cast<size_t>(max_num_frames)));
-
-  for (const auto &frame_index : frame_indices) {
-    const auto &frame = frames.at(frame_index);
-
-    object_points.push_back(object_point);
-
-    std::vector<cv::Point2f> image_point;
-    for (const auto &point : frame) {
-      image_point.push_back(cv::Point2f(point.point.x, point.point.y));
-    }
-
-    image_points.push_back(image_point);
+  if (pimpl->calib_node) {
+    pimpl->calib_node->calibrate();
   }
-
-  std::vector<cv::Mat> rvecs, tvecs;
-  cv::Mat camera_matrix = cv::Mat::eye(3, 3, CV_64F);
-  cv::Mat dist_coeffs = cv::Mat::zeros(5, 1, CV_64F);
-
-  rms = cv::calibrateCamera(object_points, image_points, image_size, camera_matrix, dist_coeffs,
-                            rvecs, tvecs);
-
-  calibrated_camera.intrin.fx = camera_matrix.at<double>(0, 0);
-  calibrated_camera.intrin.fy = camera_matrix.at<double>(1, 1);
-  calibrated_camera.intrin.cx = camera_matrix.at<double>(0, 2);
-  calibrated_camera.intrin.cy = camera_matrix.at<double>(1, 2);
-  calibrated_camera.intrin.coeffs[0] = dist_coeffs.at<double>(0);
-  calibrated_camera.intrin.coeffs[1] = dist_coeffs.at<double>(1);
-  calibrated_camera.intrin.coeffs[2] = dist_coeffs.at<double>(2);
-  calibrated_camera.intrin.coeffs[3] = dist_coeffs.at<double>(3);
-  calibrated_camera.intrin.coeffs[4] = dist_coeffs.at<double>(4);
-  calibrated_camera.width = image_width;
-  calibrated_camera.height = image_height;
 }
 
 class axis_reconstruction {
