06e0c0e1ccec8898dd34a10530ccbfb21782c574
yoshou <yoshou.dev@gmail.com>
2025-05-24 10:03:35 +0000

    refactor: Add namespace

--- lib/calibration/calibration.hpp
+++ lib/calibration/calibration.hpp
@@ -6,31 +6,11 @@
 #include "bundle_adjust_data.hpp"
 #include "parameters.hpp"
 
+namespace stargazer::calibration {
 struct observed_points_t {
   size_t camera_idx;
   std::vector<glm::vec2> points;
 };
-
-void zip_points(const std::vector<observed_points_t> &points1,
-                const std::vector<observed_points_t> &points2,
-                std::vector<std::pair<glm::vec2, glm::vec2>> &corresponding_points);
-
-void zip_points(const std::vector<observed_points_t> &points1,
-                const std::vector<observed_points_t> &points2,
-                const std::vector<observed_points_t> &points3,
-                std::vector<std::tuple<glm::vec2, glm::vec2, glm::vec2>> &corresponding_points);
-
-float compute_diff_camera_angle(const glm::mat3 &r1, const glm::mat3 &r2);
-
-glm::mat4 estimate_relative_pose(
-    const std::vector<std::pair<glm::vec2, glm::vec2>> &corresponding_points,
-    const stargazer::camera_t &base_camera, const stargazer::camera_t &target_camera,
-    bool use_lmeds = false);
-
-glm::mat4 estimate_pose(
-    const std::vector<std::tuple<glm::vec2, glm::vec2, glm::vec2>> &corresponding_points,
-    const stargazer::camera_t &base_camera1, const stargazer::camera_t &base_camera2,
-    const stargazer::camera_t &target_camera);
 
 class observed_points_frames {
   mutable std::mutex frames_mtx;
@@ -51,11 +31,31 @@ class observed_points_frames {
   void add_frame_points(uint32_t timestamp, std::string name, const std::vector<glm::vec2> &points);
 };
 
-bool initialize_cameras(std::unordered_map<std::string, stargazer::camera_t> &cameras,
+void zip_points(const std::vector<observed_points_t> &points1,
+                const std::vector<observed_points_t> &points2,
+                std::vector<std::pair<glm::vec2, glm::vec2>> &corresponding_points);
+
+void zip_points(const std::vector<observed_points_t> &points1,
+                const std::vector<observed_points_t> &points2,
+                const std::vector<observed_points_t> &points3,
+                std::vector<std::tuple<glm::vec2, glm::vec2, glm::vec2>> &corresponding_points);
+
+float compute_diff_camera_angle(const glm::mat3 &r1, const glm::mat3 &r2);
+
+glm::mat4 estimate_relative_pose(
+    const std::vector<std::pair<glm::vec2, glm::vec2>> &corresponding_points,
+    const camera_t &base_camera, const camera_t &target_camera, bool use_lmeds = false);
+
+glm::mat4 estimate_pose(
+    const std::vector<std::tuple<glm::vec2, glm::vec2, glm::vec2>> &corresponding_points,
+    const camera_t &base_camera1, const camera_t &base_camera2, const camera_t &target_camera);
+
+bool initialize_cameras(std::unordered_map<std::string, camera_t> &cameras,
                         const std::vector<std::string> &camera_names,
                         const observed_points_frames &observed_frames);
 
 void prepare_bundle_adjustment(const std::vector<std::string> &camera_names,
-                               const std::unordered_map<std::string, stargazer::camera_t> &cameras,
+                               const std::unordered_map<std::string, camera_t> &cameras,
                                const observed_points_frames &observed_frames,
-                               stargazer::calibration::bundle_adjust_data &ba_data);
+                               bundle_adjust_data &ba_data);
+};  // namespace stargazer::calibration
