68c1a3f2ce924a1d4289298a9497623c80027cde
yoshou <yoshou.dev@gmail.com>
2025-12-13 12:00:05 +0000

    refactor: Move object_mux_node and sentinel_message to separate header

--- app/calibration_pipeline.cpp
+++ app/calibration_pipeline.cpp
@@ -18,6 +18,7 @@
 #include "graph_proc_img.h"
 #include "messages.hpp"
 #include "object_map_node.hpp"
+#include "object_mux_node.hpp"
 #include "parameters.hpp"
 #include "pattern_board_calibration_target_detector_node.hpp"
 #include "reconstruction.hpp"
@@ -29,41 +30,6 @@ using namespace stargazer;
 using namespace stargazer::calibration;
 using namespace stargazer::reconstruction;
 using namespace coalsack;
-
-class sentinel_message : public graph_message {
- public:
-  sentinel_message() {}
-
-  static std::string get_type() { return "sentinel"; }
-
-  template <typename Archive>
-  void serialize(Archive& archive) {}
-};
-
-class object_mux_node : public graph_node {
-  graph_edge_ptr output;
-
- public:
-  object_mux_node() : graph_node(), output(std::make_shared<graph_edge>(this)) {
-    set_output(output);
-  }
-
-  virtual std::string get_proc_name() const override { return "object_mux"; }
-
-  virtual void process(std::string input_name, graph_message_ptr message) override {
-    if (auto obj_msg = std::dynamic_pointer_cast<object_message>(message)) {
-      for (const auto& [name, field] : obj_msg->get_fields()) {
-        auto msg = std::make_shared<object_message>();
-        msg->add_field(name, field);
-        output->send(msg);
-      }
-      auto msg = std::make_shared<sentinel_message>();
-      output->send(msg);
-    }
-  }
-};
-
-COALSACK_REGISTER_NODE(object_mux_node, graph_node)
 
 class calibration_pipeline::impl {
  public:
